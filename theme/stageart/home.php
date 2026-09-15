08eca1177955928b3d307805cdfc4b37d3b3cf66
nisehatakiti <nisehatakiti@gmail.com>
2026-09-16 05:49:05 +0900

    revert: remove accidental theme changes

--- theme/stageart/home.php
+++
@@ -1,6 +0,0 @@
-<?php
-/*
- * Posts index fallback. Keep the StageArt presentation even when WordPress
- * is configured to display latest posts on the front page.
- */
-require __DIR__ . '/front-page.php';
