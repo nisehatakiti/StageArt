058196e8d172b70265b9ef69160f3bf6c11dce07
nisehatakiti <nisehatakiti@gmail.com>
2026-09-16 05:42:49 +0900

    feat: use theatre homepage for posts index

---
+++ theme/stageart/home.php
@@ -0,0 +1,6 @@
+<?php
+/*
+ * Posts index fallback. Keep the StageArt presentation even when WordPress
+ * is configured to display latest posts on the front page.
+ */
+require __DIR__ . '/front-page.php';
